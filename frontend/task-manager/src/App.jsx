68182a74f8545912bc2a3a8f0b7073f9b57ab49d
Ayush <singhayush2999@gmail.com>
2026-08-21 17:13:40 +0530

    notifiaction+ prodfile section

--- frontend/task-manager/src/App.jsx
+++ frontend/task-manager/src/App.jsx
@@ -5,11 +5,13 @@ import Sidebar from "./components/Sidebar";
 import StatsCards from "./components/StatsCards";
 import MyTasks from "./components/MyTasks";
 import CreateTask from "./components/CreateTask";
+import SetTheme from "./components/SetTheme";
 
 function App() {
   const [tasks, setTasks] = useState([]);
   const [showCreateTask, setShowCreateTask] = useState(false);
   const [refreshTasks, setRefreshTasks] = useState(false);
+  const [darkMode,setDarkMode]=useState(false)
 
   const fetchTasks = async () => {
     try {
@@ -28,20 +30,60 @@ function App() {
   }, [refreshTasks]);
 
   return (
-    <div className="flex min-h-screen bg-gray-50">
-
+  <div
+  className={`flex min-h-screen ${
+    darkMode
+      ? "bg-gray-900"
+      : "bg-white"
+  }`}
+>
       <Sidebar />
 
-      <main className="flex-1 p-10">
+      <main className="flex-1 p-10 text-white">
 
-        <h1 className="text-3xl font-bold">
-          Good morning, Ayush 👋
-        </h1>
+<div className="flex justify-between items-start">
 
-        <p className="text-gray-500 mt-2">
-          Manage your tasks and stay productive.
+  <div>
+    <h1 className="text-3xl font-bold">
+      Good morning, Ayush 👋
+    </h1>
+
+    <p className="text-gray-500 mt-2">
+      Manage your tasks and stay productive.
+    </p>
+  </div>
+
+
+  <div className="flex items-center gap-4">
+
+    {/* Notification */}
+    <button className="w-10 h-10 rounded-full bg-white border border-gray-200 flex items-center justify-center hover:bg-gray-50">
+      🔔
+    </button>
+
+
+    {/* Profile */}
+    <div className="flex items-center gap-3">
+
+      <div className="w-10 h-10 rounded-full bg-gray-900 text-white flex items-center justify-center font-semibold">
+        A
+      </div>
+
+      <div>
+        <p className="text-sm font-semibold">
+          Ayush Kumar
         </p>
 
+        <p className="text-xs text-gray-500">
+          User
+        </p>
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
         <StatsCards tasks={tasks} />
 
         <MyTasks
@@ -59,6 +101,9 @@ function App() {
             }
           />
         )}
+        <SetTheme 
+        darkMode={darkMode}
+        setDarkMode={setDarkMode}/>
 
       </main>
 
